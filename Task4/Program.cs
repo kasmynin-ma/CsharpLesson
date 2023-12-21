cfee4634025a49349da9d8710682917b0486375e
MaximK <kasmynin.m@gmail.com>
2023-12-21 21:00:20 +0400

    reshili zadachy s 5 girjyami

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 36;
+int b = 27;
+int c = 19;
+int d = 14;
+int e = 10;
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+if(d > max)
+{
+    max = d;
+}
+if(e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
